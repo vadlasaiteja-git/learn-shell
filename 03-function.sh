8ee1354858a7a6278ec27862fcc748529ea51a69
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-13 22:59:21 +0530

    Function Variables

--- 03-function.sh
+++ 03-function.sh
@@ -21,3 +21,30 @@ example_func
 # variables in functions
 # In bash shell , if we declare a variable in main program, it is accessible inside function and vice-versa
 
+
+# Input Variables
+#                < Variable Input>
+#   <Variable Input>   function     <Variable Input>
+
+#               Variables / ENV variables
+#   LHS               function          RHS
+
+example1() {
+  echo Value of y - $y
+}
+
+y=200 example1
+
+example2() {
+  echo First Argument - $1
+}
+
+example2 300
+
+
+# Input arguments
+# script 100 200
+# $1 - 100
+# $2 - 200
+# $# - 2 ( Number of arguments)
+# $* - 100 200 ( All the arguments)
