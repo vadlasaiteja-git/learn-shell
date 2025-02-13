f73d3e9906169af60d05772a2c4b2a18a6cc500e
vadlasaiteja-git <saitejav236@gmail.com>
2025-02-13 22:40:06 +0530

    Environment Variables

--- 02-variable.sh
+++ 02-variable.sh
@@ -4,7 +4,7 @@
 # var=data
 
 #Example
-a=33
+a=10
 
 # access the variable
 # Syntax: $var
@@ -14,6 +14,7 @@ echo $a
 # Variable or Functions naming.
 # We can use characters a-z, A-Z
 # We can use numbers and Underscore
+
 
 # Input Variables
 #                < Variable Input>
@@ -27,5 +28,18 @@ echo $a
 # export var=data
 
 # on command line you run export x1=100 before executing this script
-
 echo x1 - $x1
+
+# 2. LHS
+# x2=200 y=200 script
+echo x2 - $x2
+
+# the above two approaches we need to exclusively declare variable names and their data.
+# In case if we just pass the value but some variable should be assigned automatically then RHS is the approach
+
+# 3. RHS
+# script 300
+# Here variables will be assigned automatically based on positioning
+# variable 1 - 300, can be accessed by $1
+
+echo First argument - $1
